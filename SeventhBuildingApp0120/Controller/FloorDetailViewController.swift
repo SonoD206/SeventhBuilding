a7f97017b00e75acbea3ffc563a1e096e89ee23d
SonoD206 <20cm0120@jec.ac.jp>
2021-08-14 04:45:24 +0900

    FloorDetailVCの基盤のレイアウト実装

--- SeventhBuildingApp0120/Controller/FloorDetailViewController.swift
+++ SeventhBuildingApp0120/Controller/FloorDetailViewController.swift
@@ -24,81 +24,81 @@ class FloorDetailViewController: UIViewController {
     }
     
     //    MARK: -　HomeViewと同じレイアウト
-        ///floorDetailCollectionViewの初期化
-        private func setUpCollectionView() {
-            floorDetailCollectionView.delegate = self
-            floorDetailCollectionView.dataSource = self
+    ///floorDetailCollectionViewの初期化
+    private func setUpCollectionView() {
+        floorDetailCollectionView.delegate = self
+        floorDetailCollectionView.dataSource = self
+        
+        floorDetailCollectionView.register(UINib(nibName: HomeDepartmentCollectionViewCell.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: HomeDepartmentCollectionViewCell.reuseIdentifier)
+        
+        floorDetailCollectionView.register(UINib(nibName: HomeTimeTableCollectionViewCell.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: HomeTimeTableCollectionViewCell.reuseIdentifier)
+        
+        floorDetailCollectionView.register(UINib(nibName: HomeViewHeader.reuseIdentifier, bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HomeViewHeader.reuseIdentifier)
+        
+        floorDetailCollectionView.collectionViewLayout = createLayout()
+    }
     
-            floorDetailCollectionView.register(UINib(nibName: HomeDepartmentCollectionViewCell.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: HomeDepartmentCollectionViewCell.reuseIdentifier)
-    
-            floorDetailCollectionView.register(UINib(nibName: HomeTimeTableCollectionViewCell.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: HomeTimeTableCollectionViewCell.reuseIdentifier)
-    
-            floorDetailCollectionView.register(UINib(nibName: HomeViewHeader.reuseIdentifier, bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HomeViewHeader.reuseIdentifier)
-    
-            floorDetailCollectionView.collectionViewLayout = createLayout()
-        }
-    
-        /// floorDetailCollectionView全体のレイアウト構成
-        /// - Returns: 各セクションのレイアウト
-        private func createLayout() -> UICollectionViewLayout{
-            let layout = UICollectionViewCompositionalLayout {
-                (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-    
-                let departmentSectionLayoutKind = DepartmentSectionLayoutKind.allCases[sectionIndex]
-                switch departmentSectionLayoutKind {
-                case .homeDepartment:
-                    return self.createHomeDepartmentLayout()
-                case .homeDepartmentTimeTable:
-                    return self.createHomeDepartmentTimeTableLayout()
-                }
+    /// floorDetailCollectionView全体のレイアウト構成
+    /// - Returns: 各セクションのレイアウト
+    private func createLayout() -> UICollectionViewLayout{
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let departmentSectionLayoutKind = DepartmentSectionLayoutKind.allCases[sectionIndex]
+            switch departmentSectionLayoutKind {
+            case .homeDepartment:
+                return self.createHomeDepartmentLayout()
+            case .homeDepartmentTimeTable:
+                return self.createHomeDepartmentTimeTableLayout()
             }
-            return layout
         }
+        return layout
+    }
     
-        /// HomeDepartmentのレイアウト構成をする
-        private func createHomeDepartmentLayout() -> NSCollectionLayoutSection {
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
-                                                  heightDimension: .fractionalHeight(1))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+    /// HomeDepartmentのレイアウト構成をする
+    private func createHomeDepartmentLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9),
+                                               heightDimension: .fractionalHeight(0.2))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let sectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                       heightDimension: .estimated(1))
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: sectionHeaderSize,
+                                                                        elementKind: UICollectionView.elementKindSectionHeader,
+                                                                        alignment: .top)
+        let section = NSCollectionLayoutSection(group: group)
+        section.boundarySupplementaryItems = [sectionHeader]
+        section.orthogonalScrollingBehavior = .groupPaging
+        section.interGroupSpacing = 10
+        section.contentInsets = .init(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        return section
+    }
     
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9),
-                                                   heightDimension: .fractionalHeight(0.2))
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-    
-            let sectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                           heightDimension: .estimated(1))
-            let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: sectionHeaderSize,
-                                                                            elementKind: UICollectionView.elementKindSectionHeader,
-                                                                            alignment: .top)
-            let section = NSCollectionLayoutSection(group: group)
-            section.boundarySupplementaryItems = [sectionHeader]
-            section.orthogonalScrollingBehavior = .groupPaging
-            section.interGroupSpacing = 10
-            section.contentInsets = .init(top: 10, leading: 10, bottom: 10, trailing: 10)
-    
-            return section
-        }
-    
-        /// HomeDepartmentTimeTableのレイアウト構成をする
-        private func createHomeDepartmentTimeTableLayout() -> NSCollectionLayoutSection {
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
-                                                  heightDimension: .estimated(1))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-    
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
-                                                   heightDimension: .estimated(1))
-            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
-    
-            let sectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                           heightDimension: .estimated(1))
-            let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: sectionHeaderSize,
-                                                                            elementKind: UICollectionView.elementKindSectionHeader,
-                                                                            alignment: .top)
-            let section = NSCollectionLayoutSection(group: group)
-            section.boundarySupplementaryItems = [sectionHeader]
-            section.interGroupSpacing = 16
-            section.contentInsets = .init(top: 10, leading: 10, bottom: 20, trailing: 10)
-    
-            return section
-        }
+    /// HomeDepartmentTimeTableのレイアウト構成をする
+    private func createHomeDepartmentTimeTableLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .estimated(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                               heightDimension: .estimated(1))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+        
+        let sectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                       heightDimension: .estimated(1))
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: sectionHeaderSize,
+                                                                        elementKind: UICollectionView.elementKindSectionHeader,
+                                                                        alignment: .top)
+        let section = NSCollectionLayoutSection(group: group)
+        section.boundarySupplementaryItems = [sectionHeader]
+        section.interGroupSpacing = 16
+        section.contentInsets = .init(top: 10, leading: 10, bottom: 20, trailing: 10)
+        
+        return section
+    }
 }
